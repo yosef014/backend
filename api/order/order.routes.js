87389115d6402caa72db0f757300bbb425f6b5f6
yosef plishtiiv <yosef014@gmail.com>
2022-03-28 23:41:35 +0300

    fix

--- api/order/order.routes.js
+++ api/order/order.routes.js
@@ -20,8 +20,8 @@ router.get("/:id", getOrder);
 router.put("/:id", updateOrder);
 // router.put("/:id", requireAuth, updateOrder);
 // router.post("/", updateOrder);
-router.post("/", requireAuth, addOrder);
-// router.post("/", addOrder);
+// router.post("/", requireAuth, addOrder);
+router.post("/", addOrder);
 
 // router.put('/:id',  requireAuth, updateOrder)
 router.delete("/:id", requireAuth, requireAdmin, deleteOrder);
